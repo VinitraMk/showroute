283baba4d480c4ae11701d126b1e6c660c50283d
VinitraMk <vinitramk@gmail.com>
2018-03-11 08:36:46 +0530

    debug3

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var PAGE_ACCESS_TOKEN =  process.env.PAGE_ACCESS_TOKEN;
-var APIAI_TOKEN = process.env.APIAI_TOKEN;
+//var PAGE_ACCESS_TOKEN =  process.env.PAGE_ACCESS_TOKEN;
+//var APIAI_TOKEN = process.env.APIAI_TOKEN;
+var PAGE_ACCESS_TOKEN="EAAWkJ8cZAZCpUBABttsI5sZCE6dLDDoQ4nwSTrUlCWDOSeCFcDeeJdV9QPZBEmaCPdipzg4ZBKB7Grh8NWEg8gPtBmcwgcwly73xJuhUdHz5j4JSDERsJ1ZCiusQqIYohbZCph9BIPylDcJBixZCRZBB0ZCPPE3Pza5aC6vQ7QrlcRFspvM0Tz8fI6"
+var APIAI_TOKEN="e649f8bb7b244a3b832c62d4e3c861eb"
 
 var express=require('express');
 var bodyparser=require('body-parser');
 var apiai=require('apiai');
 var app=express();
 var feedback=require('./feedback.js');
-//feedback();
-
 
 // var MongoClient = require('mongodb').MongoClient;
 
